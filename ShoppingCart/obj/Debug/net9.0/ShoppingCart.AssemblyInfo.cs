02c5eb9405c5b755cd911a4becd6e09b8d2631ea
quoclam204 <nguyenlequoclam@gmail.com>
2026-03-20 10:28:53 +0700

    fix: ignore .vs, bin, obj

--- ShoppingCart/obj/Debug/net9.0/ShoppingCart.AssemblyInfo.cs
+++ ShoppingCart/obj/Debug/net9.0/ShoppingCart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfb962d1e8a6f182cfdef01174dfde42f8f40e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1e0a74d23af79e7c4b97d1f08d7c9449a3ae51")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
