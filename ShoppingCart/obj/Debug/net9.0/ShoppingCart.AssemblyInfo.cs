5f42f52e4b1f8de60e4220f3fa243d5c682e67eb
quoclam204 <nguyenlequoclam@gmail.com>
2026-03-28 21:54:44 +0700

    Fix: ckeditor

--- ShoppingCart/obj/Debug/net9.0/ShoppingCart.AssemblyInfo.cs
+++ ShoppingCart/obj/Debug/net9.0/ShoppingCart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f1cfc5dfadb807be6b56d1df1130e7b52c3a9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da47b0906f958a7bf25cde4642c7ddcb71609226")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
