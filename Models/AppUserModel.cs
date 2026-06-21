8fd8bb5e63eec737c0d3f1503758198539a50387
quoclam204 <nguyenlequoclam@gmail.com>
2026-06-21 09:59:47 +0700

    feat: add identity and migration

---
+++ Models/AppUserModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ShoppingCart.Models
+{
+    public class AppUserModel : IdentityUser
+    {
+        public string Occupation { get; set; }
+    }
+}
