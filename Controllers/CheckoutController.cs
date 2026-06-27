5aadf9e93540bf572757a94d66d2ed35794d21d1
quoclam204 <nguyenlequoclam@gmail.com>
2026-06-28 01:41:47 +0700

    feat: checkout controller

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using ShoppingCart.Repository;
+using System.Security.Claims;
+
+namespace ShoppingCart.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public CheckoutController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Checkout()
+        {
+            // FindFirstValue: lấy thông tin người dùng đang đăng nhập
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            if (userEmail == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                // tạo ra mã Random và không bao giờ trùng
+                var oderCode = Guid.NewGuid().ToString();
+                var oderItem = new OrderModel();
+                oderItem.OrderCode = oderCode;
+                oderItem.UserName = userEmail;
+                oderItem.Status = 1;
+                oderItem.CreatedDate = DateTime.Now;
+
+                _dataContext.Add(oderItem);
+                _dataContext.SaveChanges();
+                TempData["Success"] = "Đặt hàng thành công!";
+                return RedirectToAction("Index", "Cart");
+            }
+            return View();
+        }
+    }
+}
