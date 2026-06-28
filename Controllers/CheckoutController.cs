c92a7f3b0df86135c966aafb3f2c05576066e240
quoclam204 <nguyenlequoclam@gmail.com>
2026-06-28 17:11:58 +0700

    feat: add oder-details

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -30,6 +30,7 @@ namespace ShoppingCart.Controllers
             }
             else
             {
+                // Thông tin đơn hàng
                 // tạo ra mã Random và không bao giờ trùng
                 var oderCode = Guid.NewGuid().ToString();
                 var oderItem = new OrderModel();
@@ -40,10 +41,27 @@ namespace ShoppingCart.Controllers
 
                 _dataContext.Add(oderItem);
                 _dataContext.SaveChanges();
+
+                // Chi tiết đơn hàng
+                // lấy dữ liệu giỏ hàng từ session
+                List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+                foreach (var cart in cartItems)
+                {
+                    var orderDetails = new OrderDetails();
+                    orderDetails.UserName = userEmail;
+                    orderDetails.OrderCode = oderCode;
+                    orderDetails.ProductId = cart.ProductId;
+                    orderDetails.Price = cart.Price;
+                    orderDetails.Quantity = cart.Quantity;
+
+                    _dataContext.Add(orderDetails);
+                    _dataContext.SaveChanges();
+                }    
+
+                HttpContext.Session.Remove("Cart"); 
                 TempData["Success"] = "Đặt hàng thành công!";
                 return RedirectToAction("Index", "Cart");
             }
-            return View();
         }
     }
 }
---
+++ Migrations/20260628095320_editLongProductId.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingCart.Migrations
+{
+    /// <inheritdoc />
+    public partial class editLongProductId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ProductId",
+                table: "OrderDetails",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "OrderDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
