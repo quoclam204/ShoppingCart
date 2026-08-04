ea308704fad03eaa6ca6b35cc623b368d711ba0f
quoclam204 <nguyenlequoclam@gmail.com>
2026-08-05 01:32:41 +0700

    feat: Filter by price

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace ShoppingCart.Controllers
         }
 
         // Lấy danh sách sản phẩm theo Category (dựa vào Slug) và hiển thị ra View
-        public async Task<IActionResult> Index(string Slug = "")
+        public async Task<IActionResult> Index(string Slug = "", string sort_by = "", string startprice = "", string endprice = "")
         {
             CategoryModel category = _dataContext.Categories.Where(c => c.Slug == Slug).FirstOrDefault();
 
@@ -24,10 +24,60 @@ namespace ShoppingCart.Controllers
                 return RedirectToAction("Index");
             }
 
-            // Lấy tất cả sản phẩm theo category
-            var productsByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+            // Lấy tất cả sản phẩm theo danh mục(category)
+            // IQueryable: tạo truy vấn dữ liệu nhưng chưa thực thi (chưa lấy dữ liệu từ Database)
+            IQueryable<ProductModel> productsByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
 
-            return View(await productsByCategory.OrderByDescending(p => p.Id).ToListAsync());
+            var cout = await productsByCategory.CountAsync();
+            if (cout > 0)
+            {
+                /*
+                 * Id càng lớn → sản phẩm được thêm càng mới.
+                 * Id càng nhỏ → sản phẩm được thêm càng cũ.
+                 */
+
+                if (sort_by == "price_increase") // Giá tăng dần
+                {
+                    productsByCategory = productsByCategory.OrderBy(p => p.Price);
+                }
+                else if (sort_by == "price_decrease") // Giá giảm dần
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(p => p.Price);
+                }
+                else if (sort_by == "price_newest") // Sản phẩm mới nhất (Id lớn nhất trước).
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+                }
+                else if (sort_by == "price_oldest") // Sản phẩm cũ nhất (Id nhỏ nhất trước).        
+                {
+                    productsByCategory = productsByCategory.OrderBy(p => p.Id);
+                }
+                // Lọc giá sản phẩm
+                else if (startprice != "" && endprice != "")
+                {
+                    decimal startPriceValue;
+                    decimal endPriceValue;
+
+                    // Chuyển đổi kiểu dữ liệu của giá từ string sang decimal
+                    if (decimal.TryParse(startprice, out startPriceValue) && decimal.TryParse(endprice, out endPriceValue))
+                    {
+                        // Lọc theo giá trong khoảng...
+                        productsByCategory = productsByCategory.Where(p => p.Price >= startPriceValue && p.Price <= endPriceValue);
+                    }
+                    else
+                    {
+                        // Nếu ko lọc giá thì sắp xếp theo Id giảm dần
+                        productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+                    }    
+                }
+                // Nếu ko lọc   
+                else
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+                }    
+            }
+
+            return View(await productsByCategory.ToListAsync());
         }
     }
 }
